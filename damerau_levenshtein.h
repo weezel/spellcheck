b470b66c33e18e20dbf8e67393c14226c59b6833
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2014-06-26 01:58:49 +0300

    Crap, of course I had to forget this one before pushing.

---
+++ damerau_levenshtein.h
@@ -0,0 +1,10 @@
+#ifndef _DAMERAU_LEVENHSTHEIN_H_
+#define	_DAMERAU_LEVENHSTHEIN_H_
+#endif
+
+void		usage(void);
+int		min(const int, const int);
+unsigned int	levensteinDistance(const char *, const size_t, const char *,
+				   const size_t);
+void		convertlowercase(char *, const size_t);
+void		comparefile(const char *, const char *, const size_t);
