a7000d176daf03358c5eb3f42f41fce2f197b059
Ville Valkonen <ville.oskari.valkonen@uta.fi>
2014-06-30 00:38:04 +0300

    Consistent naming

--- damerau_levenshtein.h
+++ damerau_levenshtein.h
@@ -1,6 +1,5 @@
-#ifndef _DAMERAU_LEVENHSTHEIN_H_
-#define _DAMERAU_LEVENHSTHEIN_H_
-#endif
+#ifndef __DAMERAU_LEVENHSTHEIN_H__
+#define __DAMERAU_LEVENHSTHEIN_H__
 
 void		usage(void);
 int		min(const int, const int);
@@ -8,3 +7,5 @@ unsigned int	levensteinDistance(const char *, const size_t, const char *,
 				   const size_t);
 void		convertlowercase(char *, const size_t);
 void		comparefile(const char *, const char *, const size_t);
+
+#endif
